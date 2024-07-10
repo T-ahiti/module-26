2dd7efcec3d21dd86ba8ad62ad3a3b6303a67051
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-10 23:53:50 +0600

    withdraw

--- withdraw.js
+++ withdraw.js
@@ -1,1 +1,9 @@
 
+document.getElementById('btnWithdraw').addEventListener('click', function(){
+    const withdrawField = document.getElementById('withdrawField')
+    const newDepositAmount = withdrawField.value
+    const totalWithdraw = document.getElementById('withdrawTotal')
+    const previousWithdraw = withdrawTotal.innerText
+    withdrawTotal.innerText = totalWithdraw
+    
+})
