1db5344f730fcbf513277a0ac7cc8286b69e0758
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-10 18:35:20 +0600

    pass email get

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('btnSubmit').addEventListener('click', function(){
+    // get email
+   const emailFeild = document.getElementById('userEmail')
+   const email = emailFeild.value
+    // get password
+    const passFeild = document.getElementById('userPassword')
+    const pass = passFeild.value
+})
